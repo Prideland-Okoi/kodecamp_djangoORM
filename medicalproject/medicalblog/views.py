b5e2fe0ebefaee509783a32fee30c59f9577aded
Prideland-Okoi <prideland.okoi@gmail.com>
2022-06-10 11:06:59 +0100

    updates

---
+++ medicalproject/medicalblog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse, request
+
+def home_view(request):
+    return render(request, 'home.html')
+
+def about_view(request):
+    return render(request, 'about.html')
+
+def contact_view(request):
+    return render(request, 'contact.html')
+
+# Create your views here.
