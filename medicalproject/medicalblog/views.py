78b20780f8f91f023a8639359ac6cde1e80d0083
Prideland-Okoi <prideland.okoi@gmail.com>
2022-06-10 14:51:36 +0100

    task updated

--- medicalproject/medicalblog/views.py
+++ medicalproject/medicalblog/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse, request
 
 def home_view(request):
-    return render(request, 'home.html')
+    return render(request, 'index.html')
 
 def about_view(request):
     return render(request, 'about.html')
 
 def contact_view(request):
     return render(request, 'contact.html')
+
+def contact_view(request):
+    return render(request, 'services.html')
 
 # Create your views here.
